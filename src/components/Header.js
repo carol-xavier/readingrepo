9e08b35475bdbffb26bea785e1c633e6d85b0674
carol-xavier <xavier.biotec@gmail.com>
2025-01-29 15:01:02 -0300

    Add location-dependent Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
+import { useLocation } from 'react-router-dom';
 import "./../style/reset.css";
 import "./../style/style.css";
 
+
+
 export default function Header() {
+    const location = useLocation()
+
     return (
         <div className="Header">
+           
                 <h1>Diário de Leitura</h1>
                 <button>sobre mim</button>
+            
+            {location.pathname === "/ficha-do-livro" ? (
+                <div className='BookTitle'>Titleeeeeeeeeeeeeeeeeee</div>
+            ) : (<p></p>)}
         </div>
     );
 }
