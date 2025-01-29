cebcca47cb95cf7ac55072503bd2c2e0552662a9
carol-xavier <xavier.biotec@gmail.com>
2025-01-29 14:26:22 -0300

    Organize div positions in BookPage

--- src/components/BookPage.js
+++ src/components/BookPage.js
@@ -22,11 +22,11 @@ function Checkbox() { /* It does not need to be dynamic like this once I have th
     }
 
     return (
-        <div>
-            <h3>Choose options:</h3>
+        <div className="BookContent">
+            <p>Choose options:</p>
             {options.map((option) => (
                 <label key={option}>
-                    <input 
+                    <input
                         type="checkbox"
                         value={option}
                         checked={selectedOptions.includes(option)}
@@ -41,22 +41,25 @@ function Checkbox() { /* It does not need to be dynamic like this once I have th
 
 export default function BookPage() {
     return (
-        <div className="Body">
+        <div className="BookPageBody">
             <Header />
-            <div>
-                <p>Autor:</p>
-                <p>Editora:</p>
-                <p>Ano:</p>
-                <p>Páginas:</p>
-                <p>Gênero:</p>
-                <p>Nacionalidade:</p>
+            <div className="BookPageSummary">
+                <div>
+                    <p>Autor:</p>
+                    <p>Editora:</p>
+                    <p>Ano:</p>
+                    <p>Páginas:</p>
+                    <p>Gênero:</p>
+                    <p>Nacionalidade:</p>
+                </div>
+                <Checkbox />
             </div>
-            <Checkbox />
-            <div>
-                <p>Resumo do Livro</p>
+            <div className="BookReview">
+                <h3>Resumo do Livro</h3>
                 {/*get info from database*/}
-                <p>Personagens Principais</p>
-                <p>Citação Preferida</p>
+                <p>Data Science from Zero" essentially refers to starting a journey to learn data science with no prior experience, meaning you would begin by acquiring fundamental knowledge in statistics, programming languages like Python, data manipulation techniques, and gradually progress to more advanced concepts like machine learning and data visualization to eventually analyze and extract meaningful insights from large datasets, all while building practical skills through projects and applications</p>
+                <h3>Personagens Principais</h3>
+                <h3>Citação Preferida</h3>
             </div>
             <Footer />
         </div>
