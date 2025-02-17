3e7a92149bc331a30e5324e5e270ce7167c8c59f
carol-xavier <xavier.biotec@gmail.com>
2025-02-17 10:46:46 -0300

    Style: adjust padding in book content

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import styled from 'styled-components';
 import "./../style/reset.css";
 import "./../style/style.css";
 
-export default function Book () {
+export default function Book() {
     const [books, setBooks] = useState([]);
     const URL = process.env.REACT_APP_API_URL;
 
@@ -26,13 +27,15 @@ export default function Book () {
                 books.map(book => {
                     const { _id, img, title, author } = book;
                     return (
-                        <Link className="BookContent" 
+                        <Link className="BookContent"
                             key={_id}
                             to={`/ficha-do-livro/${_id}`}
-                            >
-                            <img className="BookImage" src={img} />
-                            <h2>{title}</h2>
-                            <h2>{author}</h2>
+                        >
+                            <BookImage src={img} />
+                            <Identifier>
+                                <h2>{title}</h2>
+                                <h2>{author}</h2>
+                            </Identifier>
                         </Link>
                     )
                 })
@@ -40,3 +43,21 @@ export default function Book () {
         </div>
     );
 }
+
+const BookImage = styled.img`
+    width: 12vw; 
+    height: 12vw; 
+    border-radius: 50%; 
+    object-fit: cover;
+    margin-left: 5vw;
+    margin-right: 5vw; 
+`
+
+const Identifier = styled.div`
+    padding-top: 1.5vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
